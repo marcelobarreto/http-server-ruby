c03e9f45dfbe573907d28bd932047e5d74d19871
Marcelo Barreto <marcelobarretojunior@gmail.com>
2024-05-06 17:47:01 -0400

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -5,8 +5,37 @@ require_relative "http_status"
 require_relative "request"
 require_relative "router/router"
 
+router = Router::Router.new
+router.add_route("GET", "/") do |req|
+  [HTTPStatus::OK, {}, "Hello, World!"]
+end
+router.add_route("GET", /\/echo\/(.*)/) do |req|
+  msg = req.path.match(/\/echo\/(.*)/)[1]
+  [HTTPStatus::OK, {}, msg]
+end
+router.add_route("GET", "/user-agent") do |req|
+  msg = req.headers[2].split(": ")[1].strip
+  [HTTPStatus::OK, {}, msg]
+end
+router.set_fallback  do |req|
+  [HTTPStatus::NotFound, {}, "Not found"]
+end
+
 class HTTPServer
   attr_reader :request, :host, :port, :routes
+
+  class << self
+    def listen(port:, router:, host: "localhost")
+      server = new(port: 4221)
+      server.set_router(router)
+
+      loop do
+        Thread.start(server.accept) do |session|
+          server.respond!
+        end
+      end
+    end
+  end
 
   def initialize(port:, host: "localhost")
     puts "Listening to http://#{host}:#{port}"
@@ -14,13 +43,19 @@ class HTTPServer
     @host = host
     @port = port
     @server = TCPServer.new(host, port)
-    @socket, @addr = @server.accept
-    @request = Request.new(@socket)
+    @request = nil
+    @socket = nil
+    @addr = nil
     @router = nil
   end
 
   def set_router(router)
     @router = router
+  end
+
+  def accept
+    @socket, @addr = @server.accept
+    @request = Request.new(socket)
   end
 
   def respond!
@@ -45,23 +80,4 @@ class HTTPServer
   attr_reader :server, :socket, :addr
 end
 
-http = HTTPServer.new(port: 4221)
-
-router = Router::Router.new
-router.add_route("GET", "/") do |req|
-  [HTTPStatus::OK, {}, "Hello, World!"]
-end
-router.add_route("GET", /\/echo\/(.*)/) do |req|
-  msg = req.path.match(/\/echo\/(.*)/)[1]
-  [HTTPStatus::OK, {}, msg]
-end
-router.add_route("GET", "/user-agent") do |req|
-  msg = req.headers[2].split(": ")[1].strip
-  [HTTPStatus::OK, {}, msg]
-end
-router.set_fallback  do |req|
-  [HTTPStatus::NotFound, {}, "Not found"]
-end
-
-http.set_router(router)
-http.respond!
+HTTPServer.listen(port: 4221, host: "localhost", router: router)

6cc76ccfdf5a9b126330f05ed0ca9ae3d64985a2
Marcelo Barreto <marcelobarretojunior@gmail.com>
2024-05-06 19:18:34 -0400

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -1,41 +1,10 @@
 require "socket"
-
-require_relative "app"
 require_relative "http_status"
 require_relative "request"
 require_relative "router/router"
 
-router = Router::Router.new
-router.add_route("GET", "/") do |req|
-  [HTTPStatus::OK, {}, "Hello, World!"]
-end
-router.add_route("GET", /\/echo\/(.*)/) do |req|
-  msg = req.path.match(/\/echo\/(.*)/)[1]
-  [HTTPStatus::OK, {}, msg]
-end
-router.add_route("GET", "/user-agent") do |req|
-  msg = req.headers[2].split(": ")[1].strip
-  [HTTPStatus::OK, {}, msg]
-end
-router.set_fallback  do |req|
-  [HTTPStatus::NotFound, {}, "Not found"]
-end
-
 class HTTPServer
-  attr_reader :request, :host, :port, :routes
-
-  class << self
-    def listen(port:, router:, host: "localhost")
-      server = new(port: 4221)
-      server.set_router(router)
-
-      loop do
-        Thread.start(server.accept) do |session|
-          server.respond!
-        end
-      end
-    end
-  end
+  attr_reader :host, :port, :router
 
   def initialize(port:, host: "localhost")
     puts "Listening to http://#{host}:#{port}"
@@ -43,41 +12,52 @@ class HTTPServer
     @host = host
     @port = port
     @server = TCPServer.new(host, port)
-    @request = nil
-    @socket = nil
-    @addr = nil
-    @router = nil
+    @router = Router::Router.new
+    configure_router
   end
 
-  def set_router(router)
-    @router = router
-  end
-
-  def accept
-    @socket, @addr = @server.accept
-    @request = Request.new(socket)
-  end
-
-  def respond!
-    status, _, response = @router.respond!(request)
-
-    write!("HTTP/1.1 #{status}")
-
-    write!("Content-Type: text/plain")
-    write!("Content-Length: #{response.size || 0}")
-    write!("")
-    write!(response)
-
-    socket.close
+  def start
+    loop do
+      Thread.start(@server.accept) do |session|
+        handle_session(session)
+      end
+    end
   end
 
   private
 
-  def write!(msg)
-    socket.write(msg + "\r\n")
+  def handle_session(session)
+    request = Request.new(session)
+    status, _, response = @router.respond!(request)
+
+    session.print("HTTP/1.1 #{status}\r\n")
+    session.print("Content-Type: text/plain\r\n")
+    session.print("Content-Length: #{response.size || 0}\r\n")
+    session.print("\r\n")
+    session.print(response)
+  rescue IOError, Errno::EPIPE => e
+    puts e.message
+  ensure
+    session.close
   end
 
-  attr_reader :server, :socket, :addr
+  def configure_router
+    @router.add_route("GET", "/") do |_req|
+      [HTTPStatus::OK, {}, ""]
+    end
+    @router.add_route("GET", %r{/echo/(.*)}) do |req|
+      msg = req.path.match(%r{/echo/(.*)})[1]
+      [HTTPStatus::OK, {}, msg]
+    end
+    @router.add_route("GET", "/user-agent") do |req|
+      msg = req.headers["User-Agent"]
+      [HTTPStatus::OK, {}, msg]
+    end
+    @router.set_fallback do |_req|
+      [HTTPStatus::NotFound, {}, "Not found"]
+    end
+  end
 end
 
-HTTPServer.listen(port: 4221, host: "localhost", router: router)
+server = HTTPServer.new(port: 4221, host: "localhost")
+server.start

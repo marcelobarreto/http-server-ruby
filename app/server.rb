317ea072fd15db9da283c570a14830a4a1e78193
Marcelo Barreto <marcelobarretojunior@gmail.com>
2024-05-06 13:50:32 -0400

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -1,4 +1,24 @@
 require "socket"
 
-server = TCPServer.new("localhost", 4221)
-client_socket, client_address = server.accept
+require_relative "app"
+
+class HTTPServer
+  def initialize(port:, host: "localhost")
+    @host = host
+    @port = port
+    @server = TCPServer.new(host, port)
+    @socket, @addr = @server.accept
+  end
+
+  def write(header:)
+    # socket.write(response.join("\r\n") + "\r\n")
+    socket.write("HTTP/1.1 200 OK\r\n\r\n")
+  end
+
+  private
+
+  attr_reader :host, :port, :server, :socket, :addr
+end
+
+http = HTTPServer.new(port: 4221)
+http.write(header: "HTTP/1.1 200 OK")
